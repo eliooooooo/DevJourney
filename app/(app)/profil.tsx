4a805de94eaa729032ab590d461a09abe19241ef
eliooooooo <eliott.burkle@gmail.com>
2025-03-03 12:17:25 +0100

    Chores: remove Prebuild + setting prod build

--- app/(app)/profil.tsx
+++ app/(app)/profil.tsx
@@ -1,6 +1,7 @@
 import { useSession } from '../../context/ctx';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
+import Widget from '@/components/WidgetModule';
 
 export default function Index() {
   const { signOut } = useSession();
@@ -12,6 +13,7 @@ export default function Index() {
         }}>
         Sign Out
       </ThemedText>
+      <Widget />
     </ThemedView>
   );
 }
