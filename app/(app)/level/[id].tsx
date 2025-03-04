7e1be084c7dc4917360b864ce8b5f2100e4185cc
eliooooooo <eliott.burkle@gmail.com>
2025-03-04 17:08:45 +0100

    Fix: editable button/input after submission, lowercase error

--- app/(app)/level/[id].tsx
+++ app/(app)/level/[id].tsx
@@ -57,7 +57,9 @@ export default function LevelPage() {
   }, [level]);
 
   const handleSubmit = async (submit) => {
-    if (submit === level.answer) {
+    console.log(submit);
+    console.log(level.answer);
+    if (submit.toLowerCase() === level.answer.toLowerCase()) {
       setFeedback("Correct answer!");
       update({
         _id: userId,
@@ -73,17 +75,17 @@ export default function LevelPage() {
         const response = await axios.post(
           'https://api.openai.com/v1/chat/completions',
           {
-            model: "gpt-4o-mini",
-            messages: [{
-              role: "assistant",
-              content: `Explain in 2 phrases, why the answer "${submit}" is incorrect for the question "${level.question}". The correct answer is "${level.answer}".`
-            }],
-            max_tokens: 100
+            "model": "gpt-4o-mini",
+            "messages": [{
+                "role": "assistant",
+                "content": `Explain in 2 phrases, why the answer "${submit}" is incorrect for the question "${level.question}". The correct answer is "${level.answer}".`
+                }],
+            "max_tokens": 100
           },
           {
             headers: {
               'Content-Type': 'application/json',
-              'Authorization': `Bearer YOUR_OPENAI_API_KEY`,
+              'Authorization': `Bearer API_KEY`,
             },
           }
         );
@@ -119,11 +121,11 @@ export default function LevelPage() {
       <ThemedView>
         {level.type === "boolean" ?
           <ThemedView style={{ paddingVertical: 32, display: 'flex', flexDirection: 'row', alignItems: 'stretch' }}>
-            <TouchableOpacity onPress={() => setValue("true")} style={{ backgroundColor: value === "true" ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopLeftRadius: 30, borderBottomLeftRadius: 30, flex: 1 }}>
+            <TouchableOpacity onPress={() => setValue("true")} style={{ backgroundColor: value === "true" ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopLeftRadius: 30, borderBottomLeftRadius: 30, flex: 1 }} disabled={submited}>
               <ThemedText style={{ color: value === "true" ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>True</ThemedText>
             </TouchableOpacity>
             <View style={{ width: 1, backgroundColor: colorScheme === "dark" ? '#1E1E1E' : "#EAEAEA" }} />
-            <TouchableOpacity onPress={() => setValue("false")} style={{ backgroundColor: value === "false" ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopRightRadius: 30, borderBottomRightRadius: 30, flex: 1 }}>
+            <TouchableOpacity onPress={() => setValue("false")} style={{ backgroundColor: value === "false" ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopRightRadius: 30, borderBottomRightRadius: 30, flex: 1 }} disabled={submited}>
               <ThemedText style={{ color: value === "false" ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>False</ThemedText>
             </TouchableOpacity>
           </ThemedView>
@@ -131,21 +133,21 @@ export default function LevelPage() {
         {level.type === "multiple" ?
           <ThemedView style={{ paddingVertical: 32, display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
             <ThemedView style={{ display: 'flex', flexDirection: 'row', alignItems: 'stretch' }}>
-              <TouchableOpacity onPress={() => setValue(multiple[0])} style={{ backgroundColor: value === multiple[0] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopLeftRadius: 30, flex: 1 }}>
+              <TouchableOpacity onPress={() => setValue(multiple[0])} style={{ backgroundColor: value === multiple[0] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopLeftRadius: 30, flex: 1 }} disabled={submited}>
                 <ThemedText style={{ color: value === multiple[0] ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>{multiple[0]}</ThemedText>
               </TouchableOpacity>
               <View style={{ width: 1, backgroundColor: colorScheme === "dark" ? '#1E1E1E' : "#EAEAEA" }} />
-              <TouchableOpacity onPress={() => setValue(multiple[1])} style={{ backgroundColor: value === multiple[1] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopRightRadius: 30, flex: 1 }}>
+              <TouchableOpacity onPress={() => setValue(multiple[1])} style={{ backgroundColor: value === multiple[1] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderTopRightRadius: 30, flex: 1 }} disabled={submited}>
                 <ThemedText style={{ color: value === multiple[1] ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>{multiple[1]}</ThemedText>
               </TouchableOpacity>
             </ThemedView>
             <View style={{ height: 1, backgroundColor: colorScheme === "dark" ? '#1E1E1E' : "#EAEAEA" }} />
             <ThemedView style={{ display: 'flex', flexDirection: 'row', alignItems: 'stretch' }}>
-              <TouchableOpacity onPress={() => setValue(multiple[2])} style={{ backgroundColor: value === multiple[2] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderBottomLeftRadius: 30, flex: 1 }}>
+              <TouchableOpacity onPress={() => setValue(multiple[2])} style={{ backgroundColor: value === multiple[2] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderBottomLeftRadius: 30, flex: 1 }} disabled={submited}>
                 <ThemedText style={{ color: value === multiple[2] ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>{multiple[2]}</ThemedText>
               </TouchableOpacity>
               <View style={{ width: 1, backgroundColor: colorScheme === "dark" ? '#1E1E1E' : "#EAEAEA" }} />
-              <TouchableOpacity onPress={() => setValue(multiple[3])} style={{ backgroundColor: value === multiple[3] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderBottomRightRadius: 30, flex: 1 }}>
+              <TouchableOpacity onPress={() => setValue(multiple[3])} style={{ backgroundColor: value === multiple[3] ? '#5174F4' : colorScheme === "dark" ? '#EAEAEA' : "#F6F6F6", padding: 12, borderBottomRightRadius: 30, flex: 1 }} disabled={submited}>
                 <ThemedText style={{ color: value === multiple[3] ? 'white' : 'black', fontSize: 16, textAlign: 'center' }}>{multiple[3]}</ThemedText>
               </TouchableOpacity>
             </ThemedView>
@@ -157,7 +159,9 @@ export default function LevelPage() {
               placeholder="Your answer"
               placeholderTextColor={'gray'}
               onChangeText={(text) => setValue(text.toLowerCase())}
+              value={value}
               style={{ padding: 10, borderColor: 'gray', borderWidth: 1, color: inputText, width: '100%' }}
+              editable={!submited} // Désactiver l'input après soumission
             />
           </ThemedView>
           : null}
@@ -168,7 +172,7 @@ export default function LevelPage() {
         </TouchableOpacity>
         <ThemedText style={{ fontSize: 16, fontWeight: 'bold' }}>{feedback}</ThemedText>
         <ThemedText style={{ fontSize: 16 }}>{explanation}</ThemedText>
-        {nextLevel !== '' && nextLevel != level.id ? <Link href={`/(app)/level/${nextLevel}`} style={{ backgroundColor: '#5174F4', padding: 12, borderRadius: 30, width: '100%', display: submited === false ? 'none' : 'flex' }}>
+        {nextLevel !== '' && nextLevel != level.id ? <Link href={`/(app)/level/${nextLevel}`} style={{ backgroundColor: '#5174F4', padding: 12, borderRadius: 30, width: '100%', display: submited === false ? 'none' : 'flex', marginTop: 16 }}>
           <ThemedText style={{ color: 'white', fontSize: 16, textAlign: 'center' }}>Next level</ThemedText>
         </Link> : null}
       </ThemedView>
